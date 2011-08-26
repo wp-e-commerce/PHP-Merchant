330bea735d54658e0a838eb857929203467439af
Gary Cao <garyc40@garyc40.com>
2011-08-26 15:44:05 +0900

    - Allow getting response string - Return null if getting a non-existent item from response.

--- common/response.php
+++ common/response.php
@@ -5,15 +5,24 @@ abstract class PHP_Merchant_Response
 	protected $is_successful = false;
 	protected $options = array();
 	protected $errors = array();
+	protected $response = '';
 	
-	abstract public function __construct( $params );
+	public function __construct( $response_str ) {
+		$this->response = $response_str;
+	}
 	
 	public function is_successful() {
 		return $this->is_successful;
 	}
 	
 	public function get( $name ) {
+		if ( ! isset( $this->options[$name] ) )
+			return null;
 		return $this->options[$name];
+	}
+	
+	public function get_response_string() {
+		return $this->response;
 	}
 	
 	public function get_errors() {
