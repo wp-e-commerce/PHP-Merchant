63fc15dd8abfc33079b511be5c56e8b5170464e1
Gary Cao <garyc40@garyc40.com>
2011-08-29 16:17:24 +0900

    Check for existing key before mapping in phpme_map()

--- common/helpers.php
+++ common/helpers.php
@@ -3,6 +3,7 @@ function phpme_map( $from_array, $map, $return_type = 'Array' ) {
 	$return = array();
 	
 	foreach ( $map as $to_key => $from_key ) {
+		if ( isset( $from_array[$from_key] ) )
 		$return[$to_key] = $from_array[$from_key];
 	}
 	
